8754d264be90826b966ad9e34d8da3066a6c8132
vine <1925130654@qq.com>
2018-12-07 19:09:56 +0800

    vuex模块化状态树

--- pages/configuration.vue
+++ pages/configuration.vue
@@ -1,13 +1,32 @@
 <template>
     <div>
         configuration
-        <button @click="$store.commit('increment')">{{ $store.state.counter }}</button>
+        <ul>
+            <li v-for="todo in todos">
+            <input type="checkbox" :checked="todo.done" @change="toggle(todo)">
+            <span :class="{ done: todo.done }">{{ todo.text }}</span>
+            </li>
+            <li><input placeholder="What needs to be done?" @keyup.enter="addTodo"></li>
+        </ul>
     </div>
 </template>
 
 <script>
+import { mapMutations } from 'vuex'
 export default {
-
+    name: 'configuration',
+    computed: {
+        todos () { return this.$store.state.todos.list }
+    },
+    methods: {
+        addTodo (e) {
+        this.$store.commit('todos/add', e.target.value)
+        e.target.value = ''
+        },
+        ...mapMutations({
+        toggle: 'todos/toggle'
+        })
+    }
 }
 </script>
 
